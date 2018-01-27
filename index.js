6e4187692df0fbc04be650cba388c10d9e18f213
CodHeK <gaganganapathyas@yahoo.in>
2018-01-28 02:48:24 +0530

    save pnr

--- index.js
+++ index.js
@@ -234,8 +234,8 @@ app.post('/webhook/', function (req, res) {
                     	var query = client.query("INSERT INTO PNR(UserID, firstname, pnr) values($1, $2, $3)", [sender, name, pnrNumber]);
                     	sendTextMessage(sender, "pnr saved, for information about your train, just type in - my train status");
                     	client.query('SELECT * FROM PNR', function(err, result) {
-                    		done();
-                    		if(err) return console.error(err);
+                    		//done();
+                    		if(err) return console.log(err);
                     		console.log(result.rows);
                     	});
                     }
