fc49c2d5c00fde6dc0500129a93671bbbb7a2bcc
CodHeK <gaganganapathyas@yahoo.in>
2018-01-28 03:49:01 +0530

    save pnr

--- index.js
+++ index.js
@@ -232,8 +232,14 @@ app.post('/webhook/', function (req, res) {
 					else if(line.split(" ")[0].match(/save/g) && line.split(" ")[1].match(/pnr/g)) {
 						var pnrNumber = line.split(" ")[2];
 						if(name != null) {
-							client.query("SELECT * FROM pnr WHERE firstname='Gagan'", function(err, result) {
-								console.log(result.rows.length)
+							client.query("SELECT * FROM pnr WHERE firstname='"+name+"'", function(err, result) {
+								if(result.rows.length > 0) {
+									sendTextMessage(sender, "You already have a pnr saved")
+								}
+								else {
+									var query = client.query("INSERT INTO pnr(UserID, firstname, pnr) values($1, $2, $3)", [sender, name, pnrNumber]);
+									sendTextMessage(sender, "pnr saved, for information about your train, just type in - my train status");
+								}
 								// if(result) {
 								// 	var query = client.query("INSERT INTO PNR(UserID, firstname, pnr) values($1, $2, $3)", [sender, name, pnrNumber]);
 								// 	sendTextMessage(sender, "pnr saved, for information about your train, just type in - my train status");
