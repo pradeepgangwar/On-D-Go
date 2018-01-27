3ace13a5017c2d2c6cd34a9b1cb2ca547d238ceb
CodHeK <gaganganapathyas@yahoo.in>
2018-01-27 11:16:07 +0530

    db support

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ app.post('/webhook/', function (req, res) {
 						sendTextMessage(sender, "Hi");
 						sendTextMessage(sender, name);
 						sendTextMessage(sender, "Whatsup?");
-						$UserName = name;
 						client.query("INSERT INTO userData(Name) values($1)", [name]);
 						query.on("row", function (row, result) {
 							result.addRow(row);
